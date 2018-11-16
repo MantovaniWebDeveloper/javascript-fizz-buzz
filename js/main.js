eb8c2ec95e539a5aa587d6e4d0f9254f4d90adcd
MantovaniWebDeveloper <ratedsuperstar84@gmail.com>
2018-11-16 14:45:04 +0100

    main.js finito ora passo all'interfaccia

--- js/main.js
+++ js/main.js
@@ -5,26 +5,27 @@ var multiploCinque = 0;
 
 while (numero <= 100) {
   console.log(numero);
-  //arrivando ai numeri multipli di 3 stampi fiz al posto
-  //del numero stesso
+  
   multiploTre = numero % 3;
   multiploCinque = numero % 5;
+
   console.log("multiplo 3: " + multiploTre);
   console.log("multiplo 5: " + multiploCinque);
-  if (multiploTre == 0) {
-    console.log("Fizz");
+  //se i numeri comabaciano sia essendo multipli di 3 che di 5
+  // al posto del numero si stampi frizz buzz
+  if (multiploCinque == 0 && multiploTre == 0) {
+    console.log("FizzBuzz");
   }
   else if (multiploCinque == 0) {
+    //arrivando ai numeri multipli di 5 stampi fiz al posto
+    //del numero stesso
     console.log("Buzz");
   }
+  else  {
+    //arrivando ai numeri multipli di 3 stampi fiz al posto
+    //del numero stesso
+    console.log("Fizz");
+  }
+
   numero ++;
 }
-
-
-
-
-
-
-
-//infine se i numeri comabaciano sia essendo multipli di 3 che di 5
-// al posto del numero si stampi frizz buzz
