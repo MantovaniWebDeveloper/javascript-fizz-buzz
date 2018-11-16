7a5d449c5c65b832860f326b36f72551913a1c72
MantovaniWebDeveloper <ratedsuperstar84@gmail.com>
2018-11-16 14:18:13 +0100

    logica scritta in italiano nel main.js

---
+++ js/main.js
@@ -0,0 +1,12 @@
+//scrivere una serie di numeri da 1 a 100
+
+//arrivando ai numeri multipli di 3 stampi fiz al posto
+//del numero stesso
+
+
+//invece arrivando ai numeri multipli di 5 stampi buzz al
+//posto del numero stesso
+
+
+//infine se i numeri comabaciano sia essendo multipli di 3 che di 5
+// al posto del numero si stampi frizz buzz
