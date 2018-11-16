8cbaf16cadac5e9839199108bf1c37de022af711
MantovaniWebDeveloper <ratedsuperstar84@gmail.com>
2018-11-16 14:37:10 +0100

    inseriti multiplo 5 e controlo funzionante con stampa in console

--- js/main.js
+++ js/main.js
@@ -1,22 +1,29 @@
 //scrivere una serie di numeri da 1 a 100
 var numero = 1;
 var multiploTre = 0;
+var multiploCinque = 0;
+
 while (numero <= 100) {
   console.log(numero);
+  //arrivando ai numeri multipli di 3 stampi fiz al posto
+  //del numero stesso
   multiploTre = numero % 3;
-  console.log("multiplo: " + multiploTre);
+  multiploCinque = numero % 5;
+  console.log("multiplo 3: " + multiploTre);
+  console.log("multiplo 5: " + multiploCinque);
   if (multiploTre == 0) {
-    console.log("friz");
+    console.log("Fizz");
+  }
+  else if (multiploCinque == 0) {
+    console.log("Buzz");
   }
   numero ++;
 }
 
-//arrivando ai numeri multipli di 3 stampi fiz al posto
-//del numero stesso
 
 
-//invece arrivando ai numeri multipli di 5 stampi buzz al
-//posto del numero stesso
+
+
 
 
 //infine se i numeri comabaciano sia essendo multipli di 3 che di 5
